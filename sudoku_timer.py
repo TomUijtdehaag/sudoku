1d0386327e07b27c2b4d83c9a4bf9856cdde1a94
Tom <t.j.l.uijtdehaag@gmail.com>
2021-10-07 15:20:57 +0200

    Added multiprocessing to timer for faster testing

--- sudoku_timer.py
+++ sudoku_timer.py
@@ -1,28 +1,33 @@
+from multiprocessing import Pool
+
 from sudoku import solve, is_valid, is_complete
 from utils import read_sudokus
 
 import time
 
+def solve_one(args):
+    solve_fn, sudoku, key = args
+    print(f'Solving {key}')
+    single_start = time.time()
+    solution = solve_fn(sudoku)
+    single_duration = time.time() - single_start
+    print(f'Done in {round(single_duration, 4)}s.\nSolution:')
+    print(solution)
+    print(f'Solution is valid: {is_valid(solution)}')
+    print(f'Solution is complete: {is_complete(solution)}')
+    print('\n')
+    return single_duration
+
 def time_solver(solve_fn):
     sudokus = read_sudokus('puzzles.txt')
+    sudokus = [(solve_fn, sudoku, key) for key, sudoku in sudokus.items()]
 
-    total_start = time.time()
+    with Pool(16) as p:
+        times = p.map(solve_one, sudokus)       
 
-    for key, sudoku in sudokus.items():
-        print(f'Solving {key}')
-        single_start = time.time()
-        solution = solve_fn(sudoku)
-        single_duration = time.time() - single_start
-        print(f'Done in {round(single_duration, 4)}s.\nSolution:')
-        print(solution)
-        print(f'Solution is valid: {is_valid(solution)}')
-        print(f'Solution is complete: {is_complete(solution)}')
-        print('\n')
-
-
-    total_duration = time.time() - total_start
+    total_duration = sum(times)
     print(f'Solved all sudokus in {round(total_duration, 4)}s.')
-    print(f'Average solving time: {round(total_duration/50, 4)}s.')
+    print(f'Average solving time: {round(total_duration/len(sudokus), 4)}s.')
 
 
 def main():
